ddfea3f0c6c5fe3529158f9cc5e8aa1fd8e8e0f7
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-23 13:12:00 +0000

    Fix build script

--- build_obsidian_plugins.sh
+++ build_obsidian_plugins.sh
@@ -6,97 +6,100 @@
 # Argument 1: the path to the root of the vault. (the folder that contains the .obsidian folder)
 # Argument 2: the path from the plugin folder (.obsidian/plugins/plugin) to its source code.
 
-echo Building plugins
+echo "Building plugins"
 
-echo Vault: $1
-echo Plugin source: $2
+echo "Vault: $1"
+echo "Plugin source: $2"
 
-cd $1 || exit 1
+cd "$1" || exit 1
 
-echo Current working directory:
+echo "Current working directory:"
 pwd
 
-echo Updating submodules
-
-git submodule update --init --recursive
+echo "Updating submodules"
+git submodule update --init --recursive || exit 1
 
 excalidraw_folder_name="obsidian-excalidraw-plugin"
 better_markdown_links_folder_name="better-markdown-links"
 filename_heading_sync_folder_name="obsidian-filename-heading-sync"
 
-for d in ./.obsidian/plugins/*/ ;
-    do (
-        echo Building plugin $d
+for d in ./.obsidian/plugins/*/ ; do
+    (
+        echo "Building plugin $d"
+        cd "$d" || exit 2
 
-        cd $d || exit 2
-
-        echo Current working directory:
+        echo "Current working directory:"
         pwd
 
-        echo Finding plugin source code
+        echo "Finding plugin source code"
+        cd "$2" || exit 3
 
-        cd $2 || exit 3
-
-        echo Current working directory:
+        echo "Current working directory:"
         pwd
 
-        echo Installing dependencies
-
+        echo "Installing dependencies"
         npm i || exit 4
 
-        if [ $d == "./plugin-repositories/$excalidraw_folder_name/" ]; then
-            echo Using Excalidraw build strategy
-
+        if [[ "$d" == *"$excalidraw_folder_name"* ]]; then
+            echo "Using Excalidraw build strategy"
             (
-                cd ./MathjaxToSVG
-
+                cd ./MathjaxToSVG || exit 4
                 npm i || exit 4
                 npm run build || exit 5
-            );
-
+            )
             npm run build || exit 5
-        elif [ $d == "./plugin-repositories/$better_markdown_links_folder_name/" ]; then
-            echo Using Obsidian Dev Utils build strategy
+        elif [[ "$d" == *"$better_markdown_links_folder_name"* ]]; then
+            echo "Using Obsidian Dev Utils build strategy"
             npx obsidian-dev-utils build || exit 5
-        elif [ $d == "./plugin-repositories/$filename_heading_sync_folder_name/" ]; then
-            echo Using yarn build strategy
+        elif [[ "$d" == *"$filename_heading_sync_folder_name"* ]]; then
+            echo "Using yarn build strategy"
             npx yarn run build || exit 5
         else
-            echo Using normal build strategy
+            echo "Using normal build strategy"
             node esbuild.config.mjs production || exit 5
         fi
 
-        cd - || exit 6
-
-        echo Making sure directories exist
-
-        echo Current working directory:
+        echo "Making sure directories exist"
+        echo "Current working directory:"
         pwd
 
-        if [ $d == "./plugin-repositories/$excalidraw_folder_name/" ]; then
-            echo Using dist folder movement strategy
+        cd -
 
-            mv ${d}/dist/main.js ./plugins/${p}main.js || exit 7
-            mv ${d}/dist/manifest.json ./plugins/${p}manifest.json || exit 7
-            mv ${d}/dist/styles.css ./plugins/${p}styles.css || exit 7
-        elif [ $d == "./plugin-repositories/$better_markdown_links_folder_name/" ]; then
-            echo Using dist/build folder movement strategy
+        if [[ "$d" == *"$excalidraw_folder_name"* ]]; then
+            echo "Using dist folder movement strategy"
 
-            mv ${d}/dist/build/main.js ./plugins/${p}main.js || exit 7
-            mv ${d}/dist/build/manifest.json ./plugins/${p}manifest.json || exit 7
+            mv "$2/dist/main.js" ./ || exit 7
+            mv "$2/dist/manifest.json" ./ || exit 7
+
+            if [[ -f "$2/dist/styles.css" ]]; then
+                mv "$2/dist/styles.css" ./ || exit 7
+            fi
+        elif [[ "$d" == *"$better_markdown_links_folder_name"* ]]; then
+            echo "Using dist/build folder movement strategy"
+
+            mv "$2/dist/build/main.js" ./ || exit 7
+            mv "$2/dist/build/manifest.json" ./ || exit 7
         else
-            echo Using standard movement strategy
+            echo "Using standard movement strategy"
 
-            mv ${d}main.js ./plugins/${p}main.js || exit 7
-            cp ${d}manifest.json ./plugins/${p}manifest.json || exit 7
-            cp ${d}styles.css ./plugins/${p}styles.css || exit 7
+            mv "$2/main.js" ./ || exit 7
+            cp "$2/manifest.json" ./ || exit 7
+
+            if [[ -f "$2/styles.css" ]]; then
+                cp "$2/styles.css" ./ || exit 7
+            fi
         fi
-    );
+    )
+    
+    exit_code=$?
+    if [[ $exit_code -ne 0 ]]; then
+        echo "Error occurred while building $d. Exiting with code $exit_code."
+        exit "$exit_code"
+    fi
 done
 
-echo Current working directory:
+echo "Current working directory:"
 pwd
 
-echo Removing possible created lock files
-
+echo "Removing possible created lock files"
 git submodule foreach git restore ./ || exit 8
