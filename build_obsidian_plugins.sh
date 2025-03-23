70c0cda636628dd471606f32b273ec9f242c96fb
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-23 12:46:22 +0000

    Massively improve build script

--- build_obsidian_plugins.sh
+++ build_obsidian_plugins.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #
-# Builds the Obsidian plugins from their source code and moves the output to the plugins folder.
-# This script assumes that you run it from the root of the vault folder.
-# This script also assumes the following directory structure:
-# vault-root/.obsidian/plugin-repositories/ -> a folder containing plugin source code in the form of git submodules.
+# Builds the Obsidian plugins inside a given vault from their source code and moves the built artifacts to their corresponding folder.
+# Usage:
+# ./build_obsidian_plugins.sh ./path/to/vault/folder ./path/from/plugin/to/source
+# Argument 1: the path to the root of the vault. (the folder that contains the .obsidian folder)
+# Argument 2: the path from the plugin folder (.obsidian/plugins/plugin) to its source code.
+
 echo Building plugins
 
 echo Vault: $1
 echo Plugin source: $2
 
-cd $1
+cd $1 || exit 1
 
 echo Current working directory:
 pwd
@@ -22,18 +24,25 @@ excalidraw_folder_name="obsidian-excalidraw-plugin"
 better_markdown_links_folder_name="better-markdown-links"
 filename_heading_sync_folder_name="obsidian-filename-heading-sync"
 
-for d in ./$2/*/ ;
+for d in ./.obsidian/plugins/*/ ;
     do (
         echo Building plugin $d
 
-        cd $d || exit
+        cd $d || exit 2
+
+        echo Current working directory:
+        pwd
+
+        echo Finding plugin source code
+
+        cd $2 || exit 3
 
         echo Current working directory:
         pwd
 
         echo Installing dependencies
 
-        npm i
+        npm i || exit 4
 
         if [ $d == "./plugin-repositories/$excalidraw_folder_name/" ]; then
             echo Using Excalidraw build strategy
@@ -41,51 +50,46 @@ for d in ./$2/*/ ;
             (
                 cd ./MathjaxToSVG
 
-                npm i
-                npm run build
+                npm i || exit 4
+                npm run build || exit 5
             );
 
-            npm run build
+            npm run build || exit 5
         elif [ $d == "./plugin-repositories/$better_markdown_links_folder_name/" ]; then
             echo Using Obsidian Dev Utils build strategy
-            npx obsidian-dev-utils build
+            npx obsidian-dev-utils build || exit 5
         elif [ $d == "./plugin-repositories/$filename_heading_sync_folder_name/" ]; then
             echo Using yarn build strategy
-            npx yarn run build
+            npx yarn run build || exit 5
         else
             echo Using normal build strategy
-            node esbuild.config.mjs production
+            node esbuild.config.mjs production || exit 5
         fi
 
-        cd ../..
+        cd - || exit 6
 
         echo Making sure directories exist
 
         echo Current working directory:
         pwd
 
-        p=${d//"./plugin-repositories/"/}
-
-        echo Creating plugin directory:
-        echo $p
-
-        mkdir -p ./plugins
-        mkdir -p ./plugins/$p
-
         if [ $d == "./plugin-repositories/$excalidraw_folder_name/" ]; then
             echo Using dist folder movement strategy
-            mv ${d}/dist/main.js ./plugins/${p}main.js
-            mv ${d}/dist/manifest.json ./plugins/${p}manifest.json
-            mv ${d}/dist/styles.css ./plugins/${p}styles.css
+
+            mv ${d}/dist/main.js ./plugins/${p}main.js || exit 7
+            mv ${d}/dist/manifest.json ./plugins/${p}manifest.json || exit 7
+            mv ${d}/dist/styles.css ./plugins/${p}styles.css || exit 7
         elif [ $d == "./plugin-repositories/$better_markdown_links_folder_name/" ]; then
             echo Using dist/build folder movement strategy
-            mv ${d}/dist/build/main.js ./plugins/${p}main.js
-            mv ${d}/dist/build/manifest.json ./plugins/${p}manifest.json
+
+            mv ${d}/dist/build/main.js ./plugins/${p}main.js || exit 7
+            mv ${d}/dist/build/manifest.json ./plugins/${p}manifest.json || exit 7
         else
             echo Using standard movement strategy
-            mv ${d}main.js ./plugins/${p}main.js
-            cp ${d}manifest.json ./plugins/${p}manifest.json
-            cp ${d}styles.css ./plugins/${p}styles.css
+
+            mv ${d}main.js ./plugins/${p}main.js || exit 7
+            cp ${d}manifest.json ./plugins/${p}manifest.json || exit 7
+            cp ${d}styles.css ./plugins/${p}styles.css || exit 7
         fi
     );
 done
@@ -95,4 +99,4 @@ pwd
 
 echo Removing possible created lock files
 
-git submodule foreach git restore ./
+git submodule foreach git restore ./ || exit 8
