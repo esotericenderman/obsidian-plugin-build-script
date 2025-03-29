84b3bbb67ea506a14097a9a3481de3f1a0d0546b
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-29 18:12:30 +0000

    Add "|| exit" where needed

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Runs the build plugins script on the test vault.
 
-pushd "$(dirname "${BASH_SOURCE[0]}")"/.. > /dev/null
+pushd "$(dirname "${BASH_SOURCE[0]}")"/.. > /dev/null || exit
 
 . ./src/install_obsidian_plugins.sh --source-only
 
 install_obsidian_plugins test source
 
-popd > /dev/null
+popd > /dev/null || exit
