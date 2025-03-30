37fd5ccbeaf5d6bd17826a7101b70a66a3624bff
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-30 12:46:41 +0100

    Reorder arguments

--- src/obsidian_plugin_build_strategies.sh
+++ src/obsidian_plugin_build_strategies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -rA obsidian_plugin_build_strategies=(
+declare -Ar obsidian_plugin_build_strategies=(
     ["obsidian-filename-heading-sync"]="yarn"
     ["better-markdown-links"]="obsidian-dev-utils"
     ["obsidian-excalidraw-plugin"]="excalidraw"
